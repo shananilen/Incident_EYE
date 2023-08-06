0b20766893784d4042dca354829f47e9f8a94f88
Shanan Ilen <39610635+shananilen@users.noreply.github.com>
2023-08-06 16:38:20 +0100

    Update network_config_check.py

--- network_config_check.py
+++ network_config_check.py
@@ -35,22 +35,35 @@ def network_config_check():
     for line in route_output.splitlines():
         print(f"  {line}")
 
-    # Check for unexpected DNS servers
-    print("\nDo you want to check for unexpected DNS servers? (yes/no)")
-    choice = input("Enter your choice: ")
+    #Check for rouge DNS Servers
+    while True:
+        print("\nDo you want to check for unexpected DNS servers? (yes/no)")
+        choice = input("Enter your choice: ")
+        if choice.lower() in ['yes', 'no']:
+            break
+        else:
+            print("Invalid input. Please enter 'yes' or 'no'.")
+
     if choice.lower() == 'yes':
         expected_dns_servers = ['8.8.8.8', '8.8.4.4']  # Replace with expected DNS servers
         for server in dns.resolver.get_default_resolver().nameservers:
             if server not in expected_dns_servers:
                 print(f"WARNING: Unexpected DNS server {server}")
 
-    # Check for open ports
-    print("\nDo you want to check for open ports? (yes/no)")
-    choice = input("Enter your choice: ")
+    #Check for Open ports
+    while True:
+        print("\nDo you want to check for open ports? (yes/no)")
+        choice = input("Enter your choice: ")
+        if choice.lower() in ['yes', 'no']:
+            break
+        else:
+            print("Invalid input. Please enter 'yes' or 'no'.")
+
     if choice.lower() == 'yes':
         expected_ports = [80, 443]  # Replace with expected open ports
         for conn in psutil.net_connections():
             if conn.status == 'LISTEN' and conn.laddr.port not in expected_ports:
                 print(f"WARNING: Unexpected open port {conn.laddr.port}")
+
                 
  
